1ea485fdda09f417f0bedad58ed6d78bd15f443e
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-07 01:36:34 +0900

    update

--- src/network/http.js
+++ src/network/http.js
@@ -1,27 +1,30 @@
-import axios from "axios";
 export default class HttpClient {
   constructor(baseURL) {
     this.baseURL = baseURL;
   }
 
   async fetch(url, options) {
-    try{
-      const res = await axios(`${this.baseURL}${url}`,{
-        ...options,
-        headers: {
+    const res = await fetch(`${this.baseURL}${url}`, {
+      ...options,
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
         'Content-Type': 'application/json',
         ...options.headers,
-      }});
-      return res.json()
-    }catch(error){
+      },
+    });
+    let data;
+    try {
+      data = await res.json();
+    } catch (error) {
       console.error(error);
-      return error.response
-        ? {
-            status: error.response.status,
-            message: error.response.data.message,
-            url: error.config.url,
-          }
-        : { error: error.message };
     }
+
+    if (res.status > 299 || res.status < 200) {
+      const message =
+        data && data.message ? data.message : 'Something went wrong! 🤪';
+      throw new Error(message);
+    }
+    return data;
   }
 }
