a1ec29253b49d499daac50c4d9ba7d1fd5b983fc
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-06 17:41:26 +0900

    update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import HttpClient from './network/http';
 import TokenStorage from './db/token';
 
 // const baseURL = process.env.REACT_APP_BASE_URL;
-const baseURL = "http://internal-back-tier-alb-1256278427.ap-northeast-2.elb.amazonaws.com:8080";
+const baseURL = "http:localhost:8080";
 const tokenStorage = new TokenStorage();
 const httpClient = new HttpClient(baseURL);
 const authErrorEventBus = new AuthErrorEventBus();
