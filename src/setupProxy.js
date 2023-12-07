7356578b9953d52aa1561f75669d5c83591b44f7
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-07 13:03:58 +0900

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         createProxyMiddleware('/auth', {
             target: 'http://internal-back-tier-alb-1945782263.ap-northeast-2.elb.amazonaws.com', //접속하려는 서버의 루트 URL
-            changeOrigin: true
+            changeOrigin: false
         })
     );
     app.use(
