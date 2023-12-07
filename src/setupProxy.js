3fda1d7c1243a11572cdeb98a75158f8da5955de
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-07 15:11:09 +0900

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,13 +6,8 @@ module.exports = function(app) {
             target: 'http://internal-back-tier-alb-1945782263.ap-northeast-2.elb.amazonaws.com', //접속하려는 서버의 루트 URL
             changeOrigin: true,
             onProxyReq: (proxyReq, req, res) => {
-                if (req.method === 'POST' && req.body) {
-                  const bodyData = JSON.stringify(req.body);
-                  proxyReq.setHeader('Content-Type', 'application/json');
-                  proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
-                  proxyReq.write(bodyData);
-                  console.log(bodyData);
-                }}
+                console.log('Request payload:', req.body);
+              },
         })
     )
 };
