0f937012eadcdfcaab6a41688f29ee4c71130496
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-07 12:42:14 +0900

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        createProxyMiddleware('/', {
+        createProxyMiddleware('/auth', {
+            target: 'http://internal-back-tier-alb-1945782263.ap-northeast-2.elb.amazonaws.com', //접속하려는 서버의 루트 URL
+            changeOrigin: true
+        })
+    );
+    app.use(
+        createProxyMiddleware('/tweets', {
             target: 'http://internal-back-tier-alb-1945782263.ap-northeast-2.elb.amazonaws.com', //접속하려는 서버의 루트 URL
             changeOrigin: true
         })
