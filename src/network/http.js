58a87b05e0451bf05317094ce9d35d7128855421
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-10 12:49:28 +0900

    update

--- src/network/http.js
+++ src/network/http.js
@@ -11,6 +11,7 @@ export default class HttpClient {
       credentials: 'include',
       headers: {
         'Content-Type': 'application/json',
+        'Accept': 'application/json',
         ...options.headers,
       },
     });
