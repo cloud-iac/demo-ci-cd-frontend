cb40286c135a2d45c35ef4a923c191f3f0b15209
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-11 13:17:46 +0900

    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { AuthErrorEventBus } from './context/AuthContext';
 import HttpClient from './network/http';
 import TokenStorage from './db/token';
 
-const baseURL = "http://front-tier-alb-684012605.ap-northeast-2.elb.amazonaws.com";
+const baseURL = "http://tecc.cloud";
 const tokenStorage = new TokenStorage();
 const httpClient = new HttpClient(baseURL);
 const authErrorEventBus = new AuthErrorEventBus();
