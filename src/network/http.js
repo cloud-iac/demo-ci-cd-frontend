dafd76236ac4cec86e6e5d7a9885654dcec6797f
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-18 20:17:34 +0900

    update

--- src/network/http.js
+++ src/network/http.js
@@ -4,7 +4,7 @@ export default class HttpClient {
   }
 
   async fetch(url, options) {
-    const res = await fetch(`${this.baseURL}${url}`, {
+    const res = await fetch(url, {
       ...options,
       headers: {
         "Content-Type": "application/json",
